1819d49c605e5e1f69b1a8e3367fe48a90f8c667
Yulia Oliynyk <Lia.oliynyk6@gmail.com>
2022-06-28 14:23:45 +0300

    add frontend basic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Navigate,
+  Routes,
+} from 'react-router-dom';
 import './App.css';
+import { Login } from './pages/Login';
+import { MetaApi } from './api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [status, setStatus] = useState('waiting');
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    (async () => {
+      const api = await MetaApi.getInstance();
+      const result = await api.auth.restore({ token }).catch(() => {
+        setStatus('error');
+        return;
+      });
+      setStatus(result.status);
+    })().catch((e) => console.log(e));
+  });
+
+  if (status === 'not logged') {
+    return (
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+              <Route path="*" element={<Navigate to="/login" />} />
+            </Routes>
+          </Router>
+        </div>
+    );
+  }
+
+  if (status === 'logged') {
+    return (
+        <div className="App">
+          <Router>
+            <Routes>
+              <Route path="/mainPage" element={<>MAIN PAGE</>} />
+              <Route path="*" element={<Navigate to="/mainPage" />} />
+            </Routes>
+          </Router>
+        </div>
+    );
+  }
 }
 
 export default App;
