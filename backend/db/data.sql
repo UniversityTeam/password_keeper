7c624b325587022263cccf7fba4b292e7441c23c
Stepan Polishchuk <54286411+Professor108@users.noreply.github.com>
2022-06-28 13:36:42 +0300

    Update data.sql

--- backend/db/data.sql
+++ backend/db/data.sql
@@ -20,17 +20,3 @@ INSERT INTO "AccountRole" ("accountId", "roleId") VALUES
   (2, 2),
   (3, 2),
   (4, 2);
-
-INSERT INTO "Country" ("name") VALUES
-  ('Soviet Union'),
-  ('People''s Republic of China'),
-  ('Vietnam'),
-  ('Cuba');
-
-INSERT INTO "City" ("name", "countryId") VALUES
-  ('Beijing', 2),
-  ('Wuhan', 2),
-  ('Kiev', 1),
-  ('Havana', 4),
-  ('Hanoi', 3),
-  ('Kaliningrad', 1);
